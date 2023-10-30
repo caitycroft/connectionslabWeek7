b4bccda89c6362035c5717f72029f7009e50cc2f
Caity Croft <106709245+caitycroft@users.noreply.github.com>
2023-10-29 23:55:05 -0400

    week 7 latest updates

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-let express = require(‘express’);
+let express = require('express');
 let app = express();
+app.use(express.static('public'));
+app.use(express.json());
+
+let danceTracker = [];
 
 app.get('/', (request, response) => {
-    response.send("Hello");
+    response.send("Howdy!");
 });
 
-app.listen(3000, ( ) => {
-    console.log("app is listening at localhost:3000");	
+app.listen(5000, () => {
+    console.log("app is listening at localhost:5000");
 });
 
+//2. add a route on the server that is listening for a post request
+app.post('/noDanceParties', (request, response) => {
+    console.log(request.body);
+    response.json({ task: "success" });
+    let CurrentDate = Date.now();
+    let obj = {
+        date: CurrentDate,
+        dance: request.body.number,
+    }
+    danceTracker.push(obj);
+    console.log(danceTracker);
+    response.json({ task: "success" });
+});
+
+//add route to get all dance party tracking information
+app.get('/getDancing', (request, response) => {
+    let obj = { data: dancePartyTracker };
+    response.json(obj);
+})
+
+
+//An app.post() route on the server to listen for a POST request from the client to receive and store your data on the server
---
+++ public/app.js
@@ -0,0 +1,54 @@
+window.addEventListener('load', () => {
+    document.getElementByIDI('button_dance').addEventListener('click', () => {
+        let noDance = document.getElementById('number_danceParties').value;
+        console.log(noDance);
+
+
+        //creating the object
+        let obj = { "number": noDance };
+
+        //stringify the object
+        let jsonData = JSON.stringify(obj);
+
+
+        //fetch to route noDance -- includes meta-information about information we're sending
+        fetch('/noDance   ', {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+
+            },
+            //the request in the body is the most important part 
+            body: jsonData
+
+        })
+            .then(response => response.json())
+            .then(data => { console.log(data) });
+        //if it works there should be a console log that says "task success" on the server side 
+        //b/c we wrote that in index.js
+
+
+    });
+
+    document.getElementById('et-dance-tracker').addEventListener('click', () => {
+        //get info on ALL the dance parties we've had so far
+        fetch('/getDancing')
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('dance-info').innerHTML = '';
+                for (let i = 0; <data.data.length; i++) {
+                    let string = data.data[i].date + " : " + data.data[i].dances;
+                    let elt = document.createElement('p');
+                    elt.innerHTML = string;
+                    document.getElementById('dance-info').appendChild(elt);
+                }
+            })
+    });
+
+
+//The use of fetch() on the client to make a POST request to your server
+
+
+//The use of fetch() on the client to make a GET request from your server
+
+//An app.get() route on the server to listen for a GET request from the client to send all of the data that is stored in your database to the client
